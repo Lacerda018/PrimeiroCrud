70bba3c63d1abfc5f283422e24c5541f75bb6ae6
Lucas Lacerda <lucaslacerdaandrey@gmail.com>
2024-02-22 16:52:13 -0300

    implementação de banco de dados, instalação PDO_MYSQL

--- app/database/Connect.php
+++ app/database/Connect.php
@@ -7,6 +7,7 @@ use PDO;
 class Connect
 {
     public static function connect()
+
     {
         return new PDO('mysql:host=localhost;dbname=google_login', 'root', '', [
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
